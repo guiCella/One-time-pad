a22780fe39c3f515e46094e35f92f37cc625c06b
Guilherme Luiz Cella <guilhermecella8@gmail.com>
2024-06-04 19:52:06 -0300

    Added comments on the code to be more visable to non developers

--- app/Http/Controllers/VernamCipherController.php
+++ app/Http/Controllers/VernamCipherController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\VernamCipher;
 use Illuminate\Support\Facades\Storage;
-use setasign\Fpdi\Fpdi;
-use TCPDF;
+
 
 class VernamCipherController extends Controller
 {
@@ -18,27 +17,27 @@ class VernamCipherController extends Controller
     }
 
     public function encryptFile(Request $request)
-{
-    $request->validate([
-        'file' => 'required|file',
-        'key' => 'required|string'
-    ]);
+    {
+        $request->validate([
+            'file' => 'required|file',
+            'key' => 'required|string'
+        ]);
 
-    $file = $request->file('file');
-    $key = $request->input('key');
-    $contents = file_get_contents($file->getRealPath());
+        $file = $request->file('file');
+        $key = $request->input('key');
+        $contents = file_get_contents($file->getRealPath());
 
-    $key = str_pad($key, strlen($contents), $key, STR_PAD_RIGHT);
+        $key = str_pad($key, strlen($contents), $key, STR_PAD_RIGHT);
 
-    $encryptedContents = $this->cipher->encrypt($contents, $key);
+        $encryptedContents = $this->cipher->encrypt($contents, $key);
 
-    $originalFileName = $file->getClientOriginalName();
-    $filePath = storage_path('app/' . $originalFileName);
+        $originalFileName = $file->getClientOriginalName();
+        $filePath = storage_path('app/' . $originalFileName);
 
-    Storage::disk('local')->put($originalFileName, $encryptedContents);
+        Storage::disk('local')->put($originalFileName, $encryptedContents);
 
-    return response()->download($filePath, $originalFileName, ['Content-Type' => 'text/plain'])->deleteFileAfterSend(true);
-}
+        return response()->download($filePath, $originalFileName, ['Content-Type' => 'text/plain'])->deleteFileAfterSend(true);
+    }
 
     public function showForm()
     {
